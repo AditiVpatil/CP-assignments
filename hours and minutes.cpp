d82a906de25696e4eb4c90176a3284ef1eb2a9e0
Aditi Patil <patiladiti2206@gmail.com>
2024-10-17 20:06:15 +0530

    Add files via upload

---
+++ hours and minutes.cpp
@@ -0,0 +1,13 @@
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{
+    int hours,minutes;
+    cout<<"Enter hours:"<<endl;
+    cin>>hours;
+cout<<"Enter minutes"<<endl;
+cin>>minutes;
+   int Total=hours*60+minutes;
+    cout<<"Total:"<<Total;
+    return 0;
+}
